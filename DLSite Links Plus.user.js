055b2c13710f634fa5e0ee222adc46efd8e2ff95
kanige <>
2025-05-16 06:51:04 -0500

    preview images work

--- DLSite Links Plus.user.js
+++ DLSite Links Plus.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name        DLSite Links+ dev
+// @name        DLSite Links+
 // @namespace   Loli-A-Best
 // @match       *://boards.4chan.org/*/thread/*
 // @match       *://boards.4channel.org/*/thread/*
